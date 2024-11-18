e9a1e5da0d1d1fbcb3b4be6f80611873757b7277
Luis Rodolfo Navarro <navaluisrodolfo@gmail.com>
2024-11-18 09:11:16 -0300

    add disconnet when user connecten in other instance

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,20 +1,64 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
-  handleConnection(client: Socket) {
-    console.log('client connected', client.id);
+  @WebSocketServer() wss: Server;
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    this.wss.emit('client-updated', this.messagesWsService.getConnectClient());
   }
   handleDisconnect(client: Socket) {
-    console.log('client disconnected', client.id);
+    this.messagesWsService.removeClient(client.id);
+
+    this.wss.emit('client-updated', this.messagesWsService.getConnectClient());
   }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto): void {
+    //!Emite solo al client
+    // client.emit('message-from-server', {
+    //   fullName: 'Server',
+    //   message: payload.message || 'no-message',
+    // });
+
+    //!Emite a todos MENOS al client inicial
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Server',
+    //   message: payload.message || 'no-message',
+    // });
+
+    //!Emite a todos
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id) ,
+      message: payload.message || 'no-message',
+    });
+  }
+
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Socket } from 'socket.io';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
+
+interface ConnectedClient {
+  [id: string]: {
+    socket: Socket;
+    user: User;
+  };
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+  private connectedClient: ConnectedClient = {};
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async registerClient(client: Socket, userId: string) {
+    const user = await this.userRepository.findOneBy({ id: userId });
+
+    if (!user) throw new Error('User not found');
+
+    if (!user.isActive) throw new Error('User not active');
+
+    this.checkUserConnection(user);
+
+    this.connectedClient[client.id] = {
+      socket: client,
+      user,
+    };
+  }
+
+  removeClient(clientId: string) {
+    delete this.connectedClient[clientId];
+  }
+
+  getConnectClient(): string[] {
+    // console.log(this.connectedClient);
+
+    return Object.keys(this.connectedClient);
+  }
+
+  getUserFullName(socketId: string) {
+    return this.connectedClient[socketId].user.fullName;
+  }
+
+  private checkUserConnection(user: User) {
+    for (const clientId of Object.keys(this.connectedClient)) {
+      const connectClient = this.connectedClient[clientId];
+      if (connectClient.user.id === user.id) {
+        connectClient.socket.disconnect();
+        break;
+      }
+    }
+  }
+}
